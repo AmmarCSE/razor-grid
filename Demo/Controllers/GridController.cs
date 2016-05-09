5c4ec3e01ead5db0becefc37f97b79c7e6283588
AmmarCSE <ammar.abu-yasein@farsightsoftware.com>
2016-05-09 02:12:27 -0700

    runnable demo

---
+++ Demo/Controllers/GridController.cs
@@ -0,0 +1,21 @@
+﻿using Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.Controllers
+{
+    public class GridController : Controller
+    {
+        //
+        // GET: /Grid/
+
+        public ActionResult Index()
+        {
+            return View("Grid", new GridModelList());
+        }
+
+    }
+}
---
+++ Demo/Models/GridModels.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Mvc.Html;
+using System.Web.Security;
+
+using AmmarCSE.RazorGrid.Resources;
+
+namespace Demo.Models
+{
+    public class GridModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Display(Name = "Age")]
+        public int? Age { get; set; }
+    }
+
+    public class GridModelList
+    {
+        public List<GridModel> Data { get; set; }
+        public List<GridEnums.GridPermission> gridPermissions { get; set; }
+        public Dictionary<GridEnums.GridPermission, string> gridActions { get; set; }
+
+        public GridModelList()
+        {
+            Data = new List<GridModel>();
+            Data.Add(new GridModel() {Name = "1", Address = "2", Age = 3 });
+            Data.Add(new GridModel() {Name = "4", Address = "5", Age = 6 });
+
+            gridPermissions = new List<GridEnums.GridPermission>();
+            gridPermissions.Add(GridEnums.GridPermission.Add);
+            gridPermissions.Add(GridEnums.GridPermission.Delete);
+            gridPermissions.Add(GridEnums.GridPermission.Update_Activation);
+
+            gridActions = new Dictionary<GridEnums.GridPermission, string>();
+            //gridActions.Add(GridEnums.GridPermission.Add, GridCustomScript.ACTION_REDIRECT("'google.com'"));
+        }
+    }
+}
