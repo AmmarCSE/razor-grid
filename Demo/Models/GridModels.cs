c5772830b49a5f2bbec6dfa404472d9a6a22a127
AmmarCSE <ammar.abu-yasein@farsightsoftware.com>
2016-05-11 23:03:52 -0700

    stabilize example

--- Demo/Models/GridModels.cs
+++ Demo/Models/GridModels.cs
@@ -15,18 +15,26 @@ namespace Demo.Models
     public class GridModel
     {
         [Required]
-        [Display(Name = "Name")]
-        [GridHtml(Attr = "style", AttrVal = "width:33%")]
-        public string Name { get; set; }
+        [Display(Name = "First Name")]
+        [GridHtml(Attr = "style", AttrVal = "width:20%")]
+        public string FirstName { get; set; }
 
         [Required]
-        [Display(Name = "Address")]
-        [GridHtml(Attr = "style", AttrVal = "width:33%")]
-        public string Address { get; set; }
+        [Display(Name = "Last Name")]
+        [GridHtml(Attr = "style", AttrVal = "width:20%")]
+        public string LastName { get; set; }
 
         [Display(Name = "Age")]
-        [GridHtml(Attr = "style", AttrVal = "width:33%")]
+        [GridHtml(Attr = "style", AttrVal = "width:13%")]
         public int? Age { get; set; }
+
+        [Display(Name = "Join Date")]
+        [GridHtml(Attr = "style", AttrVal = "width:26%")]
+        public DateTime JoinDate { get; set; }
+
+        [Display(Name = "Email")]
+        [GridHtml(Attr = "style", AttrVal = "width:20%")]
+        public string Email { get; set; }
     }
 
     public class GridModelList
@@ -37,9 +45,14 @@ namespace Demo.Models
 
         public GridModelList()
         {
+            DateTime now = DateTime.Now;
             Data = new List<GridModel>();
-            Data.Add(new GridModel() {Name = "1", Address = "2", Age = 3 });
-            Data.Add(new GridModel() {Name = "4", Address = "5", Age = 6 });
+            Data.Add(new GridModel() {FirstName = "John", LastName = "Doe", Age = 23, Email = "john@email.com", JoinDate = now.AddDays(-7) });
+            Data.Add(new GridModel() {FirstName = "Jane", LastName = "Smith", Age = 34, Email = "jane@email.com", JoinDate = now.AddYears(-7) });
+            Data.Add(new GridModel() {FirstName = "Jack", LastName = "Black", Age = 32, Email = "black@email.com", JoinDate = now.AddMonths(-7) });
+            Data.Add(new GridModel() {FirstName = "John", LastName = "Public", Age = 46, Email = "johnQ@email.com", JoinDate = now.AddDays(-2) });
+            Data.Add(new GridModel() {FirstName = "Even", LastName = "Steven", Age = 26, Email = "evensteven@email.com", JoinDate = now.AddMonths(-3) });
+            Data.Add(new GridModel() {FirstName = "Joe", LastName = "Sixpack", Age = 66, Email = "sixpack@email.com", JoinDate = now.AddDays(-77) });
 
             gridPermissions = new List<GridEnums.GridPermission>();
             gridPermissions.Add(GridEnums.GridPermission.Add);
